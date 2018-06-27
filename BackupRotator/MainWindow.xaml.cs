79062c73858b0589eb92a55a1f23a2ae9ba86a6f
Andrew Dysart <andrew@igglean.com>
2018-06-27 10:55:38 -0400

    Add some QOL features (optionally deleting backups on close being the primary one)

--- BackupRotator/MainWindow.xaml.cs
+++ BackupRotator/MainWindow.xaml.cs
@@ -31,6 +31,9 @@ namespace BackupRotator
         private int _backupInterval;
         private Timer _backupTimer;
         private bool _folderMode = false;
+
+        private delegate void UpdateBackupSelectorDelegate(int numberOfBackups);
+
         public MainWindow()
         {
             InitializeComponent();
@@ -70,20 +73,18 @@ namespace BackupRotator
             if (int.TryParse(tbBackupNum.Text, out int backupNum) && int.TryParse(tbBackupInterval.Text, out int backupInterval))
             {
                 cbBackupSelection.Items.Clear();
-                for(int i = 1; i <= backupNum; i++)
-                {
-                    cbBackupSelection.Items.Add(string.Format("{0} minutes back", i * backupInterval));
-                }
                 _backupNum = backupNum;
                 _backupInterval = backupInterval;
 
                 if (_folderMode)
                 {
                     clearExtraDirectories();
+                    makeBackupDirectory();
                 }
                 else
                 {
                     clearExtraFiles();
+                    makeBackupFile();
                 }
 
                 _backupTimer.Interval = _backupInterval * 60 * 1000;
@@ -156,18 +157,26 @@ namespace BackupRotator
             int backupIndex = lastIndex - cbBackupSelection.SelectedIndex;
             string backupTargetName = _backupTargetDirectory.FullName;
             string backupTempName = string.Format("{0}{1}", _backupTargetDirectory.FullName, "_tmp");
-            _backupTargetDirectory.MoveTo(backupTempName);
-            DirectoryInfo tempDir = new DirectoryInfo(backupTempName);
-            _backupTargetDirectory = new DirectoryInfo(backupTargetName);
-            _backupTargetDirectory.Create();
 
-            foreach(FileInfo f in _backupArrayDirectory[backupIndex].EnumerateFiles())
+            try
             {
-                string dest = string.Format("{0}\\{1}", backupTargetName, f.Name);
-                f.CopyTo(dest);
-            }
+                _backupTargetDirectory.MoveTo(backupTempName);
+                DirectoryInfo tempDir = new DirectoryInfo(backupTempName);
+                _backupTargetDirectory = new DirectoryInfo(backupTargetName);
+                _backupTargetDirectory.Create();
 
-            tempDir.Delete(true);
+                foreach (FileInfo f in _backupArrayDirectory[backupIndex].EnumerateFiles())
+                {
+                    string dest = string.Format("{0}\\{1}", backupTargetName, f.Name);
+                    f.CopyTo(dest);
+                }
+
+                tempDir.Delete(true);
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show(string.Format("Failed to restore backup! Your original directory may be found at {0}.", backupTempName));
+            }
         }
 
         private void onElapsed(object sender, EventArgs e)
@@ -185,7 +194,7 @@ namespace BackupRotator
         private void makeBackupFile()
         {
             DateTime time = DateTime.Now;
-            FileInfo newCopy = _backupTargetFile.CopyTo(string.Format("{0}_{1}-{2}", _backupTargetFile.Name, time.Hour, time.Minute));
+            FileInfo newCopy = _backupTargetFile.CopyTo(string.Format("{0}_{1}-{2}-{3}", _backupTargetFile.Name, time.Hour, time.Minute, time.Second));
             _backupArrayFile.Add(newCopy);
 
             if (_backupArrayFile.Count > _backupNum)
@@ -194,12 +203,16 @@ namespace BackupRotator
                 oldCopy.Delete();
                 _backupArrayFile.RemoveAt(0);
             }
+            else
+            {
+                Dispatcher.BeginInvoke(System.Windows.Threading.DispatcherPriority.Normal, new UpdateBackupSelectorDelegate(updateBackupSelection), _backupArrayFile.Count);
+            }
         }
 
         private void makeBackupDirectory()
         {
             DateTime time = DateTime.Now;
-            DirectoryInfo newDirectory = new DirectoryInfo(string.Format("{0}_{1}-{2}", _backupTargetDirectory.FullName, time.Hour, time.Minute));
+            DirectoryInfo newDirectory = new DirectoryInfo(string.Format("{0}_{1}-{2}-{3}", _backupTargetDirectory.FullName, time.Hour, time.Minute, time.Second));
             newDirectory.Create();
             _backupArrayDirectory.Add(newDirectory);
 
@@ -214,11 +227,43 @@ namespace BackupRotator
                 oldDirectory.Delete(true);
                 _backupArrayDirectory.RemoveAt(0);
             }
+            else
+            {
+                Dispatcher.BeginInvoke(System.Windows.Threading.DispatcherPriority.Normal, new UpdateBackupSelectorDelegate(updateBackupSelection), _backupArrayDirectory.Count);                
+            }
+        }
+
+        private void updateBackupSelection(int numberOfBackups)
+        {
+            cbBackupSelection.Items.Add(string.Format("{0} minutes back", numberOfBackups * _backupInterval));
         }
 
         private void chkFolderMode_Checked(object sender, RoutedEventArgs e)
         {
             _folderMode = (bool)chkFolderMode.IsChecked;
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            base.OnClosed(e);
+
+            if (_backupArrayDirectory.Count > 0 || _backupArrayFile.Count > 0)
+            {
+                MessageBoxResult result = MessageBox.Show("Would you like to keep the backups generated during this session?", "Before you go...", MessageBoxButton.YesNo);
+
+                if (result == MessageBoxResult.No)
+                {
+                    foreach (FileInfo f in _backupArrayFile)
+                    {
+                        f.Delete();
+                    }
+
+                    foreach (DirectoryInfo d in _backupArrayDirectory)
+                    {
+                        d.Delete(true);
+                    }
+                }
+            }
         }
     }
 }
